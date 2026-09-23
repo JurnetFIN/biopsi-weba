191fba83f2a1215889e239838e97fec28c42ba35
Juliusz Kotelba <juliusz.kotelba@gmail.com>
2026-09-23 15:49:38 +0300

    Removed hervanta office page from strapi

--- strapi/src/api/organization-office-hervanta/routes/organization-office-hervanta.ts
+++
@@ -1,7 +0,0 @@
-/**
- * organization-office-hervanta router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::organization-office-hervanta.organization-office-hervanta');
