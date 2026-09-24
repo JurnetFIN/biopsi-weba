7fc8fed7e8be2193c01233bd3d82f0be034502ec
Juliusz Kotelba <juliusz.kotelba@gmail.com>
2026-09-24 14:07:32 +0300

    Lint run

--- next/src/components/UserDropdown/CloseableLinks/CloseableLinks.tsx
+++ next/src/components/UserDropdown/CloseableLinks/CloseableLinks.tsx
@@ -14,7 +14,6 @@ export default function CloseableLink({
   lang,
   dictionary,
   isBiopsiHato,
-  isBiopsiMember,
 }: CloseableLinkProps) {
   const handleClick = () => {
     const elem = document.activeElement as HTMLElement;
